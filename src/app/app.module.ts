fa62e9854df9bc1e75f600c252c6119a1cd3bb38
kdrumm <kyle.drumm@nextcentury.com>
2017-03-01 11:24:57 -0500

    fixed linting error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ import { VisualizationInjectorComponent } from './components/visualization-injec
 import { FilterTrayComponent } from  './components/filter-tray/filter-tray.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
-import { LegendComponent } from './components/legend/legend.component'
+import { LegendComponent } from './components/legend/legend.component';
 import { ChartModule } from 'angular2-chartjs';
 
 export function createAppModule(config: NeonGTDConfig) {
