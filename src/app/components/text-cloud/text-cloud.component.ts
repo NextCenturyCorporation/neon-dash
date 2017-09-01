bd7ffd6b8f1999f412a7809d66f53b297c615eb8
Scott Warner <scott.warner@nextcentury.com>
2017-09-01 12:19:51 -0400

    Text cloud: Add support for using a 'size' field
    This allows the size of the data item in the cloud to be determined
    by the average of a field i the data.
    
    This can only be set up in the config file currently

--- src/app/components/text-cloud/text-cloud.component.ts
+++ src/app/components/text-cloud/text-cloud.component.ts
@@ -28,10 +28,12 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
         table: string,
         dataField: string,
         unsharedFilterField: any,
-        unsharedFilterValue: string
+        unsharedFilterValue: string,
+        sizeField: string
     };
     public active: {
         dataField: FieldMetaData,
+        sizeField: FieldMetaData,
         andFilters: boolean,
         limit: number,
         textColor: string,
@@ -50,11 +52,13 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
             table: this.injector.get('table', null),
             dataField: this.injector.get('dataField', null),
             unsharedFilterField: this.injector.get('unsharedFilterField', null),
-            unsharedFilterValue: this.injector.get('unsharedFilterValue', null)
+            unsharedFilterValue: this.injector.get('unsharedFilterValue', null),
+            sizeField: this.injector.get('sizeField', null)
         };
         this.filters = [];
         this.active = {
             dataField: new FieldMetaData(),
+            sizeField: new FieldMetaData(),
             andFilters: true,
             limit: 40,
             textColor: '#111',
@@ -79,15 +83,18 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
 
     subGetBindings(bindings: any) {
         bindings.dataField = this.active.dataField.columnName;
+        bindings.sizeField = this.active.sizeField.columnName;
     }
 
     getExportFields() {
+        let countField = this.active.sizeField.prettyName === '' ? 'Count' :
+            this.active.sizeField.prettyName;
         return [{
             columnName: this.active.dataField.columnName,
             prettyName: this.active.dataField.prettyName
         }, {
             columnName: 'value',
-            prettyName: 'Count'
+            prettyName: countField
         }];
     }
 
@@ -115,7 +122,9 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
 
     onUpdateFields() {
         let dataField = this.findFieldObject('dataField', neonMappings.TAGS);
+        let sizeField = this.findFieldObject('sizeField', neonMappings.TAGS);
         this.active = this.updateObject(this.active, 'dataField', dataField);
+        this.active = this.updateObject(this.active, 'sizeField', sizeField);
         this.meta = Object.assign({}, this.meta); //trigger action
     };
 
@@ -125,7 +134,7 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
     };
 
     createNeonFilterClauseEquals(_databaseAndTableName: {}, fieldName: string) {
-        let filterClauses = this.filters.map(function(filter) {
+        let filterClauses = this.filters.map((filter) => {
             return neon.query.where(fieldName, '=', filter.value);
         });
         if (filterClauses.length === 1) {
@@ -173,8 +182,17 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
             whereClause = neon.query.where(this.meta.unsharedFilterField.columnName, '=', this.meta.unsharedFilterValue);
         }
 
-        return query.where(whereClause).groupBy(dataField).aggregate(neon.query['COUNT'], '*', 'value')
-            .sortBy('value', neon.query['DESCENDING']).limit(this.active.limit);
+        if (this.active.sizeField.columnName === '') {
+            // Normal aggregation query
+            return query.where(whereClause).groupBy(dataField).aggregate(neon.query['COUNT'], '*', 'value')
+                .sortBy('value', neon.query['DESCENDING']).limit(this.active.limit);
+        } else {
+            // Query for data with the size field and sort by it
+            let sizeColumn = this.active.sizeField.columnName;
+            return query.where(neon.query.and(whereClause, neon.query.where(sizeColumn, '!=', null)))
+                .groupBy(dataField).aggregate(neon.query['AVG'], sizeColumn, sizeColumn)
+                .sortBy(sizeColumn, neon.query['DESCENDING']).limit(this.active.limit);
+        }
     };
 
     getFiltersToIgnore() {
@@ -184,14 +202,23 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
     onQuerySuccess(response): void {
         let data = response.data;
         let cloudData = data || [];
+        let useSizeField: boolean = this.active.sizeField.columnName !== '';
+
         let activeData = cloudData.map((item) => {
             item.key = item[this.active.dataField.columnName];
             item.keyTranslated = item.key;
+            // If we have a size field, asign the value to the value field
+            if (useSizeField) {
+                item.value = item[this.active.sizeField.columnName];
+            }
             return item;
         });
         this.active = this.updateObject(this.active, 'data', activeData);
         this.refreshVisualization();
         this.queryTitle = 'Text Cloud by ' + this.active.dataField.prettyName;
+        if (useSizeField) {
+            this.queryTitle += ' and ' + this.active.sizeField.prettyName;
+        }
     }
 
     setupFilters() {
