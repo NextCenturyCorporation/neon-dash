6f962d190d6e9da1d063920b8b9673e1969dade6
kbaik <kevin.baik@nextcentury.com>
2017-09-05 14:50:09 -0400

    CSS Update Textcloud and Document Viewer

--- src/app/components/text-cloud/text-cloud.component.ts
+++ src/app/components/text-cloud/text-cloud.component.ts
@@ -139,7 +139,7 @@ export class TextCloudComponent extends BaseNeonComponent implements OnInit, OnD
     };
 
     updateTextCloudSettings() {
-        let options = new TextCloudOptions(new SizeOptions(130, 250, '%'),
+        let options = new TextCloudOptions(new SizeOptions(100, 200, '%'),
             new ColorOptions('#aaaaaa', this.active.textColor));
         this.textCloud = new TextCloud(options);
     };
