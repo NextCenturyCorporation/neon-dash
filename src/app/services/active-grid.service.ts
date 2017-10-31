e86748c1a88bce0b3690dd3a05fa928e8257dfbc
Scott Warner <scott.warner@nextcentury.com>
2017-10-31 10:10:35 -0400

    Fix issues with Material B12 and angular2-grid
    For some reason with Material B12, the angular2-grid library would measure the space
    incorrectly on load, and the grid would be many times larger than the available window.
    To fix this, force it to re-measure when the page has loaded.

--- src/app/services/active-grid.service.ts
+++ src/app/services/active-grid.service.ts
@@ -218,4 +218,11 @@ export class ActiveGridService {
 
         return true;
     }
+
+    /**
+     * Tell the grid to trigger a resize event
+     */
+    triggerResize() {
+        this.grid.triggerResize();
+    }
 }
