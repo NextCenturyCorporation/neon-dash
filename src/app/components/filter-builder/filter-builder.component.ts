90f0966e4102aae0dc0132926b0241c0b729a799
Daniel Gilley <daniel.gilley@nextcentury.com>
2018-01-16 17:55:18 -0500

    Fixed issue with filter builder filters not being properly added and removed from the filter service.

--- src/app/components/filter-builder/filter-builder.component.ts
+++ src/app/components/filter-builder/filter-builder.component.ts
@@ -32,6 +32,11 @@ import * as neon from 'neon-framework';
 import { BaseNeonComponent } from '../base-neon-component/base-neon.component';
 import { VisualizationService } from '../../services/visualization.service';
 
+class FilterBuilderDatabaseTableMetadata {
+    filterId: string;
+    clauses: WhereClauseMetaData[];
+}
+
 @Component({
     selector: 'app-filter-builder',
     templateUrl: './filter-builder.component.html',
@@ -51,9 +56,11 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
     public active: {
         operators: OperatorMetaData[],
         andor: string,
-        whereClauses: WhereClauseMetaData[],
-        filterIds: string[]
+        whereClauses: Map<string, FilterBuilderDatabaseTableMetadata>,
+        whereClausesAsList: WhereClauseMetaData[]
     };
+
+    private counter: number;
 
     constructor(connectionService: ConnectionService, datasetService: DatasetService, filterService: FilterService,
         exportService: ExportService, injector: Injector, themesService: ThemesService, ref: ChangeDetectorRef,
@@ -68,9 +75,11 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
         this.active = {
             operators: [],
             andor: 'and',
-            whereClauses: [],
-            filterIds: []
+            whereClauses: new Map<string, FilterBuilderDatabaseTableMetadata>(),
+            whereClausesAsList: []
         };
+
+        this.counter = -1;
 
         this.active.operators.push({ value: '=', prettyName: '=' });
         this.active.operators.push({ value: '!=', prettyName: '!=' });
@@ -122,24 +131,59 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
             field: field,
             operator: this.active.operators[0],
             value: '',
-            active: false
+            active: false,
+            id: ++this.counter
         };
-        this.active.whereClauses.push(clause);
+        let databaseTableKey = this.getDatabaseTableKey(clause.database.name, clause.table.name);
+        if (!this.active.whereClauses[databaseTableKey]) {
+            this.active.whereClauses.set(databaseTableKey, new FilterBuilderDatabaseTableMetadata());
+            this.active.whereClauses.get(databaseTableKey).clauses = [];
+        }
+        this.active.whereClauses.get(databaseTableKey).clauses.push(clause);
+        this.active.whereClausesAsList.push(clause);
     }
 
-    removeClause(i) {
-        this.active.whereClauses.splice(i, 1);
-        if (this.active.whereClauses.length === 0) {
+    removeClause(where) {
+        let databaseTableKey = this.getDatabaseTableKey(where.database.name, where.table.name);
+        for (let i = this.active.whereClauses.get(databaseTableKey).clauses.length - 1; i >= 0; i--) {
+            let clause = this.active.whereClauses.get(databaseTableKey).clauses[i];
+            if (clause.id === where.id) {
+                this.active.whereClauses.get(databaseTableKey).clauses.splice(i, 1);
+                break;
+            }
+        }
+        for (let i = this.active.whereClausesAsList.length - 1; i >= 0; i--) {
+            if (this.active.whereClausesAsList[i].id === where.id) {
+                this.active.whereClausesAsList.splice(i, 1);
+                break;
+            }
+        }
+        if (this.active.whereClauses.get(databaseTableKey).clauses.length === 0) {
+            if (this.active.whereClauses.get(databaseTableKey).filterId) {
+                this.filterService.removeFilter(
+                    this.messenger,
+                    this.active.whereClauses.get(databaseTableKey).filterId,
+                    () => null);
+            }
+            this.active.whereClauses.delete(databaseTableKey);
+        }
+        if (this.active.whereClauses.size === 0) {
             this.addBlankWhereClause();
         }
     }
 
-    activateClause(i) {
-        this.active.whereClauses[i].active = true;
+    activateClause(where) {
+        let databaseTableKey = this.getDatabaseTableKey(where.database.name, where.table.name);
+        for (let clause of this.active.whereClauses.get(databaseTableKey).clauses) {
+            if (clause.id === where.id) {
+                clause.active = true;
+                break;
+            }
+        }
         this.updateFilters();
     }
 
-    refreshClause(_i) {
+    refreshClause(where) {
         this.updateFilters();
     }
 
@@ -148,68 +192,31 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
     }
 
     updateFilters() {
-        // This process seems unnecessarily inefficient.  I sort the clauses just so i know how many times I need to call add neon filter.
-        // Later, I need to sort them again.
-        let cls = {};
-        // organize clauses by database/field combinations
-        for (let clause of this.active.whereClauses) {
-            if (clause.active) {
-                let dt = this.getDatabaseTableKey(clause.database.name, clause.table.name);
-                let set = cls[dt];
-                if (!set) {
-                    set = [];
-                    cls[dt] = set;
-                }
-                set.push(clause);
+        this.active.whereClauses.forEach((value, key) => {
+            let hasActiveClause = false;
+            for (let index = value.clauses.length - 1; !hasActiveClause && index >= 0; index--) {
+                hasActiveClause = value.clauses[index].active;
             }
-        }
-
-        // Remove any filters that we've saved but no longer have the database/table combo
-        // figure out what filters to remove
-        let removeFilters = [];
-        for (let key in this.active.filterIds) {
-            if (this.active.filterIds.hasOwnProperty(key)) {
-                if (!cls[key]) {
-                    removeFilters.push(this.active.filterIds[key]);
-                }
+            if (hasActiveClause) {
+                this.addNeonFilter(false, value);
             }
-        }
-
-        // remove the filters
-        if (removeFilters.length > 0) {
-            this.filterService.removeFilters(
-                this.messenger,
-                removeFilters,
-                () => {
-                    // on success, clear out the filters
-                    let temp = [];
-                    for (let id of this.active.filterIds) {
-                        if (removeFilters.indexOf(id) === -1) {
-                            temp.push(id);
-                        }
-                    }
-                    this.active.filterIds = temp;
-                });
-        }
-        // add the existing filters
-        for (let key in cls) {
-            if (cls.hasOwnProperty(key)) {
-                let clauses = cls[key];
-                if (clauses && clauses.length > 0) {
-                    this.addNeonFilter(false, clauses);
-                }
-            }
-        }
+        });
     }
 
     resetFilterBuilder() {
         let callback = () => {
-            this.active.filterIds = [];
-            this.active.whereClauses = [];
+            this.active.whereClauses = new Map<string, FilterBuilderDatabaseTableMetadata>();
+            this.active.whereClausesAsList = [];
             this.active.andor = 'and';
             this.addBlankWhereClause();
         };
-        this.filterService.removeFilters(this.messenger, this.active.filterIds, callback.bind(this));
+        let filterIds = [];
+        this.active.whereClauses.forEach((value, key) => {
+            if (value.filterId) {
+                filterIds.push(value.filterId);
+            }
+        });
+        this.filterService.removeFilters(this.messenger, filterIds, callback.bind(this));
     }
 
     getDatabaseTableKey(database, table) {
@@ -219,7 +226,8 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
     /*
     * Assumes all clauses passed in have the same database/table combination
     */
-    addNeonFilter(executeQueryChainOnsuccess: boolean, clauses: WhereClauseMetaData[]) {
+    addNeonFilter(executeQueryChainOnsuccess: boolean, databaseTableMetadata: FilterBuilderDatabaseTableMetadata) {
+        let clauses = databaseTableMetadata.clauses;
         if (!clauses || clauses.length === 0) {
             return;
         }
@@ -228,17 +236,14 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
         let databaseTableKey = this.getDatabaseTableKey(database, table);
         let text = database + ' - ' + table + ' - filter';
         let visName = this.getVisualizationName();
-        let onSuccess = () => {
-            let filters = this.filterService.getFiltersByOwner(this.id);
-            for (let filter of filters) {
-                this.active.filterIds[databaseTableKey] = filter.id;
-            }
+        let onSuccess = (neonFilterId) => {
+            databaseTableMetadata.filterId = neonFilterId;
         };
         let onError = () => {
             console.error('filter failed to set');
         };
 
-        let filterId = this.active.filterIds[databaseTableKey];
+        let filterId = databaseTableMetadata.filterId;
 
         if (filterId) {
             this.filterService.replaceFilter(
@@ -274,8 +279,8 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
 
     createNeonFilterClauseEquals(database: string, table: string, fieldName: string) {
         let filterClauses = [];
-        for (let whereClause of this.active.whereClauses) {
-            if (whereClause.database.name === database && whereClause.table.name === table && whereClause.active) {
+        for (let whereClause of this.active.whereClauses.get(this.getDatabaseTableKey(database, table)).clauses) {
+            if (whereClause.active) {
                 let operator = whereClause.operator.value;
                 let value: any = whereClause.value;
                 if (operator !== 'contains' && operator !== 'not contains') {
@@ -346,11 +351,16 @@ export class FilterBuilderComponent extends BaseNeonComponent implements OnInit,
         // Do nothing, no filters
     }
 
-    handleValueChange(_event, i) {
-        if (this.active.whereClauses[i].value && this.active.whereClauses[i].value !== '') {
-            // TODO
-        } else {
-            this.active.whereClauses[i].active = false;
+    handleValueChange(_event, where) {
+        for (let clause of this.active.whereClauses.get(this.getDatabaseTableKey(where.database.name, where.table.name)).clauses) {
+            if (clause.id === where.id) {
+                if (clause.value && clause.value !== '') {
+                    // TODO
+                } else {
+                    clause.active = false;
+                }
+                return;
+            }
         }
     }
 
@@ -379,4 +389,5 @@ export class WhereClauseMetaData {
     operator: OperatorMetaData;
     value: string;
     active: boolean;
+    id: number;
 }
