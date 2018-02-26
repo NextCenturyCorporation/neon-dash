d02d971efe30a21444698ca44781f28e45a21f9f
bdpham <bdpham93@gmail.com>
2018-02-26 16:23:23 -0500

    added multi row selection in data-table component

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -47,6 +47,8 @@ export class DataTableComponent extends BaseNeonComponent implements OnInit, OnD
     @ViewChild('table') table: any;
     @ViewChild('dragView') dragView: ElementRef;
 
+    selected = [];
+
     private filters: {
         id: string,
         key: string,
@@ -560,6 +562,8 @@ export class DataTableComponent extends BaseNeonComponent implements OnInit, OnD
         if (selected && selected.length && this.active.idField.columnName && selected[0][this.active.idField.columnName]) {
             this.publishSelectId(selected[0][this.active.idField.columnName]);
         }
+        this.selected.splice(0, this.selected.length);
+        this.selected.push(...selected);
     }
 
     /**
