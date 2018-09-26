7087698c8cc1478741341a99f2c13ba27cf44fa3
Patrick Sharkey <patrick.sharkey@nextcentury.com>
2018-09-26 08:01:32 -0400

    Fixed entrypoint.sh to continue executing indefinitely.

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -4,19 +4,14 @@
 if ! ps aux | grep -q "[s]upervisor"; then
   echo "Starting supervisor service"
   /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf &
-
-  echo "creating /.es_created"
-  touch /.es_created
-
 else
   echo "Supervisor is currently running"
 fi
 
-# Check if Elasticsearch is up and insert data
+# Elasticsearch variables
 ES_DATA="/usr/local/elasticsearch/thor_data"
 ES_INDEX="ldc_uyg_jul_18"
 ES_MAPPING="ui_out"
-
 PORT=9200
 URL="http://localhost:$PORT"
 
@@ -26,18 +21,27 @@ until curl -s --output /dev/null -XGET $URL/; do
   sleep 2
 done
 
-set -x 
-# Create Elasticsearch index and mapping
-curl -XPUT "$URL/$ES_INDEX/" -d @"$ES_DATA/index-settings.json"
-curl -XPUT "$URL/$ES_INDEX/$ES_MAPPING/_mapping" -d @"$ES_DATA/${ES_INDEX}_mapping.json"
+# Verify if elasticsearch was already setup
+if [ ! -f /.es_created ]; then
+  echo "Setting up Elasticsearch index and ingesting dat"
 
-# Insert Elasticsearch data
-elasticdump \
-  --bulk=true \
-  --input="$ES_DATA/${ES_INDEX}.json" \
-  --output="$URL/$ES_INDEX"
+  # Create Elasticsearch index and mapping
+  curl -XPUT "$URL/$ES_INDEX/" -d @"$ES_DATA/index-settings.json"
+  curl -XPUT "$URL/$ES_INDEX/$ES_MAPPING/_mapping" -d @"$ES_DATA/${ES_INDEX}_mapping.json"
 
-echo "creating /.es_created"
-touch /.es_created
+  # Insert Elasticsearch data
+  elasticdump \
+    --bulk=true \
+    --input="$ES_DATA/${ES_INDEX}.json" \
+    --output="$URL/$ES_INDEX"
 
-exec "$@"
+  echo "creating /.es_created"
+  touch /.es_created
+else 
+  echo "Elasticsearch has already been setup"
+fi
+
+# We are breaking convention and standing everything up in a single container. Because of this
+# we need to end the script with a single process that continues to run forever to keep the 
+# container alive.
+tail -f /dev/null
