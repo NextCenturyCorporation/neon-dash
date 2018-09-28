051df73fd7f44524d2b58210f4a7aaa1762ceaf6
Patrick Sharkey <patrick.sharkey@nextcentury.com>
2018-09-28 12:40:26 -0400

    Added Geoserver file upload and namespace and workspace creation
    Added a bind mount point to mount the tif data in run.sh
    
    added a natural earth build to help script find data files
    
    Added first cut of the xml configurations for geoserver

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo docker run -d -p 2222:22 -p 8888:8080 -p 9999:9200 lorelei
+
+
+#sudo docker run -v ~/Desktop/data:/usr/local/geoserver/data/NE2_HR_LC_SR_W_DR -it -p 2222:22 -p 8888:8080 -p 9999:9200 lorelei
+sudo docker run -v ~/Desktop/data:/usr/local/geoserver/data/NE2_HR_LC_SR_W_DR -d -p 2222:22 -p 8888:8080 -p 9999:9200 lorelei
+
