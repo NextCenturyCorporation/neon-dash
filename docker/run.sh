e6117fc1e5646ea8cd28d42c30942784176c82be
Patrick Sharkey <patrick.sharkey@nextcentury.com>
2018-09-28 14:10:54 -0400

    Removed personal run.sh and created example run.sh
    Updated run.sh.example to include --name lorelei for containers
    
    Updated gitignore to exlude personal run.sh

--- docker/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-
-#sudo docker run -v ~/Desktop/data:/usr/local/geoserver/data/NE2_HR_LC_SR_W_DR -it -p 2222:22 -p 8888:8080 -p 9999:9200 lorelei
-sudo docker run -v ~/Desktop/data:/usr/local/geoserver/data/NE2_HR_LC_SR_W_DR -d -p 2222:22 -p 8888:8080 -p 9999:9200 lorelei
-
