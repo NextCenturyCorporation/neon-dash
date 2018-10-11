94082b3a3abc77f47aba52445ffd061938d89d4b
Kevin Baik <kevin.baik@nextcentury.com>
2018-10-11 10:18:08 -0400

    Fixed Unit Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -62,6 +62,7 @@ import { NgGridModule } from 'angular2-grid';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { AppMaterialModule } from './app.material.module';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SimpleFilterComponent } from './components/simple-filter/simple-filter.component';
 import { ChartComponent } from './components/chart/chart.component';
@@ -128,6 +129,7 @@ describe('App: NeonGtd', () => {
                 NgxGraphModule,
                 NgxDatatableModule,
                 HttpModule,
+                HttpClientModule,
                 BrowserAnimationsModule,
                 ReactiveFormsModule
             ],
