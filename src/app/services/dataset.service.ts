522386876164a343526aa67c89085605214c7429
Kevin Baik <kevin.baik@nextcentury.com>
2018-11-02 12:45:36 -0400

    Checks if SimpleFilter exists, if not create one.

--- src/app/services/dataset.service.ts
+++ src/app/services/dataset.service.ts
@@ -16,7 +16,7 @@
 import { Inject, Injectable } from '@angular/core';
 import * as neon from 'neon-framework';
 
-import { Dataset, DatasetOptions, DatabaseMetaData, TableMetaData, TableMappings, FieldMetaData, Relation } from '../dataset';
+import { Dataset, DatasetOptions, DatabaseMetaData, TableMetaData, TableMappings, FieldMetaData, Relation, SimpleFilter } from '../dataset';
 import { Subscription, Observable } from 'rxjs/Rx';
 import { NeonGTDConfig } from '../neon-gtd-config';
 import * as _ from 'lodash';
@@ -181,7 +181,23 @@ export class DatasetService {
      * @param simpleField The new field for the simple search
      */
     public setActiveDatasetSimpleFilterFieldName(simpleField: FieldMetaData) {
+        if (!this.dataset.options.simpleFilter) {
+            this.createSimpleFilter();
+        }
         this.dataset.options.simpleFilter.fieldName = simpleField.columnName;
+    }
+
+    /**
+     * Creates a simpleFilter if it doesn't exist
+     */
+    public createSimpleFilter() {
+        if (!this.dataset.options.simpleFilter) {
+            this.dataset.options.simpleFilter = new SimpleFilter(
+                this.dataset.databases[0].name,
+                this.dataset.databases[0].tables[0].name,
+                ''
+            );
+        }
     }
 
     /**
@@ -189,6 +205,9 @@ export class DatasetService {
      * @return {string}
      */
     public getActiveDatasetSimpleFilterFieldName(): string {
+        if (!this.dataset.options.simpleFilter) {
+            this.createSimpleFilter();
+        }
         return this.dataset.options.simpleFilter.fieldName;
     }
 
