666dea14ed48c9a33691f8dbdfae282c45dc4237
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2018-11-05 09:45:15 -0500

    Moved the Color and ColorSet classes from the ColorSchemeService into their own new file.

--- src/app/components/aggregation/aggregation.component.spec.ts
+++ src/app/components/aggregation/aggregation.component.spec.ts
@@ -30,7 +30,8 @@ import { LegendComponent } from '../legend/legend.component';
 import { UnsharedFilterComponent } from '../unshared-filter/unshared-filter.component';
 
 import { ActiveGridService } from '../../services/active-grid.service';
-import { Color, ColorSchemeService } from '../../services/color-scheme.service';
+import { Color } from '../../color';
+import { ColorSchemeService } from '../../services/color-scheme.service';
 import { ConnectionService } from '../../services/connection.service';
 import { DatasetService } from '../../services/dataset.service';
 import { ErrorNotificationService } from '../../services/error-notification.service';
