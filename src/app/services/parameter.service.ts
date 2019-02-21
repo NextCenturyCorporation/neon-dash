6dd29a3f721b9ade382f3beccfb165ae616eeeda
Rachel Artiss <rachel.artiss@nextcentury.com>
2019-02-21 17:11:29 -0500

    Update datastore type and host getter names for clarity.

--- src/app/services/parameter.service.ts
+++ src/app/services/parameter.service.ts
@@ -276,8 +276,8 @@ export class ParameterService {
      * @param {String} filterStateId
      */
     loadState(dashboardStateId: string | number, filterStateId: string | number) {
-        let connection: neon.query.Connection = this.connectionService.createActiveConnection(this.datasetService.getDatastore(),
-            this.datasetService.getHostname());
+        let connection: neon.query.Connection = this.connectionService.createActiveConnection(this.datasetService.getDatastoreType(),
+            this.datasetService.getDatastoreHost());
         let params: any = {};
         if (dashboardStateId) {
             params.dashboardStateId = dashboardStateId;
