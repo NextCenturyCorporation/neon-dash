559ef94c804a00d0ef2fe09343c63cec8f25c0d1
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-04-19 09:20:14 -0400

    THOR-942 Forgot to add the updates in the unit test mock services to the previous commit.

--- src/testUtils/MockServices/FilterServiceMock.ts
+++ src/testUtils/MockServices/FilterServiceMock.ts
@@ -13,66 +13,8 @@
  * limitations under the License.
  *
  */
-import * as neon from 'neon-framework';
-import * as _ from 'lodash';
-import { FilterService, ServiceFilter } from '../../app/services/filter.service';
+import { FilterService } from '../../app/services/filter.service';
 
 export class FilterServiceMock extends FilterService {
-    addFilter(messenger: neon.eventing.Messenger,
-        ownerId: string,
-        database: string,
-        table: string,
-        whereClause: any,
-        filterName: string | { visName: string; text: string },
-        onSuccess: (resp: any) => any,
-        onError: (resp: any) => any) {
-
-        // avoid network call
-        let id = database + '-' + table + '-' + filterName;
-        let filter = this.createNeonFilter(database, table, whereClause, this.getFilterNameString(database, table, filterName));
-        this.filters.push(new ServiceFilter(id, ownerId, database, table, filter));
-
-        // don't do success call to avoid calling query chain
-    }
-
-    // Override to avoid calls to the DatasetService.
-    getFilterNameString(database: string, table: string, filterName: string | {visName: string, text: string}): string {
-        if (typeof filterName === 'object') {
-            return (filterName.visName ? filterName.visName + ' - ' : '') + database + ' - ' + table +
-                (filterName.text ? ': ' + filterName.text : '');
-        }
-        return filterName;
-    }
-
-    getLatestFilterId(): string {
-        return this.filters[this.filters.length - 1].id;
-    }
-
-    replaceFilter(messenger: neon.eventing.Messenger,
-        id: string,
-        ownerId: string,
-        database: string,
-        table: string,
-        whereClause: any,
-        filterName: string | {visName: string, text: string},
-        onSuccess: (resp: any) => any,
-        onError: (resp: any) => any) {
-
-        let filter = this.createNeonFilter(database, table, whereClause, this.getFilterNameString(database, table, filterName));
-        let index = _.findIndex(this.filters, { id: id });
-        this.filters[index] = new ServiceFilter(id, ownerId, database, table, filter);
-
-        // don't do success call to avoid calling query chain
-    }
-
-    removeFilter(messenger: neon.eventing.Messenger,
-        id: string,
-        onSuccess?: (resp: any) => any,
-        onError?: (resp: any) => any) {
-
-        let index = _.findIndex(this.getFilters(), {id: id});
-        this.getFilters().splice(index, 1);
-
-        // don't do success call to avoid calling query chain
-    }
+    // TODO
 }
