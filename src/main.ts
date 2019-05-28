809ce897cc256f9fa2f05bedf30aaa2468267421
Timothy Soehnlin <tim.soehnlin@nextcentury.com>
2019-05-28 09:18:27 -0400

    Fixing issue with polyfill loading twice

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@
  * limitations under the License.
  *
  */
-import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
