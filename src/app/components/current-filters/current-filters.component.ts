ca371d6505e03ff0e32fe19d9763f8d05e9b7ce3
Timothy Soehnlin <tim.soehnlin@nextcentury.com>
2019-05-29 17:06:31 -0400

    Finishing tests up

--- src/app/components/current-filters/current-filters.component.ts
+++ src/app/components/current-filters/current-filters.component.ts
@@ -37,12 +37,93 @@ interface FilterGroup {
     filters?: FilterDisplay[];
 }
 
+export class FilterUtil {
+    static cleanValue(type: string, val: any) {
+        switch (type) {
+            case 'date': return moment(val).format('YYYY-MM-DD');
+            case 'geo': return parseFloat(val).toFixed(3);
+            default: return val;
+        }
+    }
+
+    static translateOperator(type: string, op: string) {
+        switch (type) {
+            case 'date': return op.replace(/^<=?$/, 'before').replace(/^>=?$/, 'after');
+            default: return op.replace(/^=$/g, '');
+        }
+    }
+
+    static isGeo(type: string, name: string) {
+        return type === 'double' && /[.](lat|lon)$/i.test(name);
+    }
+
+    static computeFilter(x: AbstractFilter): FilterDisplay {
+        let ret: Partial<FilterDisplay>;
+        if (x instanceof SimpleFilter) {
+            ret = {
+                field: x.field.prettyName,
+                value: this.cleanValue(x.field.type, x.value),
+                op: this.translateOperator(x.field.type, x.operator)
+            };
+        } else if (x instanceof CompoundFilter) {
+            const simples = x.filters.filter((y) => y instanceof SimpleFilter) as SimpleFilter[];
+            const latLongs = simples.filter((y) => this.isGeo(y.field.type, y.field.prettyName));
+            const dates = simples.filter((y) => y.field.type === 'date');
+
+            if (x.filters.length === 2) {
+                if (dates.length === 2) {
+                    ret = {
+                        field: dates[0].field.prettyName,
+                        op: 'between',
+                        value: `${this.cleanValue('date', dates[0].value)} and ${this.cleanValue('date', dates[1].value)}`,
+                    };
+                } else if (latLongs.length === 2) {
+                    ret = {
+                        field: latLongs[0].field.prettyName.replace(/[.](lat|lon)$/i, ''),
+                        op: 'at',
+                        value: `(${this.cleanValue('geo', latLongs[0].value)}, ${this.cleanValue('geo', latLongs[1].value)})`
+                    };
+                }
+            } else if (x.filters.length === 4) {
+                if (latLongs.length === 4) {
+                    ret = {
+                        field: latLongs[0].field.prettyName.replace(/[.](lat|lon)$/i, ''),
+                        op: 'from',
+                        value: `(${this.cleanValue('geo', latLongs[0].value)}, ${this.cleanValue('geo', latLongs[1].value)}) to (${this.cleanValue('geo', latLongs[2].value)}, ${this.cleanValue('geo', latLongs[3].value)})`
+                    };
+                }
+            }
+            if (!ret) {
+                const values = x.filters
+                    .map((v) => this.computeFilter(v).text)
+                    .join(` ${x.type} `.toUpperCase());
+
+                ret = { text: `(${values})` };
+            }
+        }
+        if (!ret.full) {
+            ret.full = x.toDesign();
+        }
+        if (!ret.text) {
+            if (ret.field) {
+                ret.text = `${ret.field} ${ret.op} ${ret.value}`.replace(/\s+/g, ' ');
+            } else {
+                ret.text = ret.full.name;
+            }
+        }
+        return ret as FilterDisplay;
+    }
+
+
+}
+
 @Component({
     selector: 'app-current-filters',
     templateUrl: './current-filters.component.html',
     styleUrls: ['./current-filters.component.scss']
 })
 export class CurrentFiltersComponent implements OnInit, OnDestroy {
+
     private messenger: eventing.Messenger;
 
     public COMPOUND_FILTER_TYPE = CompoundFilterType;
@@ -78,78 +159,11 @@ export class CurrentFiltersComponent implements OnInit, OnDestroy {
         );
     }
 
-    computeFilter(x: AbstractFilter): FilterDisplay {
-        let ret: Partial<FilterDisplay>;
-        if (x instanceof SimpleFilter) {
-            const isDate = /date/i.test(x.field.type);
-            ret = {
-                field: x.field.prettyName,
-                value: x.value,
-                op: isDate ?
-                    x.operator
-                        .replace(/<=?/, 'before')
-                        .replace(/>=?/, 'after') :
-                    x.operator
-                        .replace(/=/g, '')
-            };
-        } else if (x instanceof CompoundFilter) {
-            const latLongs = x.filters
-                .filter((y) => y instanceof SimpleFilter && y.field.type === 'double' && /[.](lat|lon)$/.test(y.field.prettyName))
-                .map((y) => (y as SimpleFilter));
-
-            const dates = x.filters
-                .filter((y) => y instanceof SimpleFilter && y.field.type === 'date')
-                .map((y) => (y as SimpleFilter));
-
-
-            if (x.filters.length === 2) {
-                if (dates.length === 2) {
-                    ret = {
-                        field: dates[0].field.prettyName,
-                        op: 'between',
-                        value: `${moment(dates[0].value).format('YYYY-MM-DD')} and ${moment(dates[1].value).format('YYYY-MM-DD')}`,
-                    };
-                } else if (latLongs.length === 2) {
-                    ret = {
-                        field: latLongs[0].field.prettyName.replace(/[.](lat|lon)$/, ''),
-                        op: 'at',
-                        value: `(${latLongs[0].value.toFixed(3)}, ${latLongs[1].value.toFixed(3)})`
-                    };
-                }
-            } else if (x.filters.length === 4) {
-                if (latLongs.length === 4) {
-                    ret = {
-                        field: latLongs[0].field.prettyName.replace(/[.](lat|lon)$/, ''),
-                        op: 'from',
-                        value: `(${latLongs[0].value.toFixed(3)}, ${latLongs[1].value.toFixed(3)}) to (${latLongs[2].value.toFixed(3)}, ${latLongs[3].value.toFixed(3)})`
-                    };
-                }
-            }
-            if (!ret) {
-                const values = x.filters
-                    .map((v) => this.computeFilter(v).text)
-                    .join(` ${x.type} `.toUpperCase());
-
-                ret = { text: `(${values})` };
-            }
-        }
-        if (!ret.full) {
-            ret.full = x.toDesign();
-        }
-        if (!ret.text) {
-            if (ret.field) {
-                ret.text = `${ret.field} ${ret.op} ${ret.value}`;
-            } else {
-                ret.text = ret.full.name;
-            }
-        }
-        return ret as FilterDisplay;
-    }
 
     updateFilters() {
         this.groups = [];
         for (const x of this.filterService.getRawFilters()) {
-            const filter = this.computeFilter(x);
+            const filter = FilterUtil.computeFilter(x);
             if (filter.field) {
                 const grp = this.groups.find((g) => g.name === filter.field);
                 if (!grp) {
