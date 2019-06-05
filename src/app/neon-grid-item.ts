27f7cc23f793303ddc1b720c8fa86e9f7bf108f1
Nicolas Brockman <nbrockman@pc-2152.hq.nextcentury.com>
2019-06-05 14:42:33 -0400

    Changed Copyright dates to 2019 from 2017

--- src/app/neon-grid-item.ts
+++ src/app/neon-grid-item.ts
@@ -1,5 +1,5 @@
-/*
- * Copyright 2017 Next Century Corporation
+/**
+ * Copyright 2019 Next Century Corporation
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,11 +11,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
-
-/**
- * The config options of an individual widget for the angular2-grid.
  */
 export interface NeonGridItem {
     bindings?: any;
