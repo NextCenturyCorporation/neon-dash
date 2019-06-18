8f42ea5462e7bbee1229e034d55d85be0b8d2ab0
Timothy Soehnlin <tim.soehnlin@nextcentury.com>
2019-06-18 18:34:11 -0400

    Resolving final test issues

--- src/app/services/dashboard.service.spec.ts
+++ src/app/services/dashboard.service.spec.ts
@@ -18,7 +18,7 @@ import { AbstractSearchService } from './abstract.search.service';
 import { NeonConfig, NeonDatastoreConfig, NeonDashboardLeafConfig, FilterConfig } from '../models/types';
 import { DashboardService } from './dashboard.service';
 
-import { initializeTestBed } from '../../testUtils/initializeTestBed';
+import { initializeTestBed, getConfigService } from '../../testUtils/initializeTestBed';
 import { DashboardServiceMock, MockConnectionService } from '../../testUtils/MockServices/DashboardServiceMock';
 import { ConfigService } from './config.service';
 import { SearchServiceMock } from '../../testUtils/MockServices/SearchServiceMock';
@@ -91,7 +91,7 @@ describe('Service: DashboardService with Mock Data', () => {
     let configService: ConfigService;
 
     beforeEach(() => {
-        configService = ConfigService.as(NeonConfig.get());
+        configService = getConfigService();
         dashboardService = new DashboardServiceMock(configService);
     });
 
