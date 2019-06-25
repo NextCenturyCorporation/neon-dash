3c1889c69c302dd4a27d44d01fb47b0ff4c9360d
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-06-25 11:30:00 -0400

    THOR-1269 Changed injectable services to extend base services.

--- src/app/services/injectable.color-theme.service.ts
+++ src/app/services/injectable.color-theme.service.ts
@@ -12,48 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Injectable } from '@angular/core';
 import { ColorThemeService } from './color-theme.service';
-import { Color, ColorSet } from '../models/color';
-import { Theme } from './abstract.color-theme.service';
 
-@Injectable()
-export class InjectableColorThemeService {
-    private _service = new ColorThemeService();
+@Injectable({
+    providedIn: 'root'
+})
+export class InjectableColorThemeService extends ColorThemeService { }
 
-    public getColor(databaseName: string, tableName: string, fieldName: string, value: string | string[]): Color {
-        return this._service.getColor(databaseName, tableName, fieldName, value);
-    }
-
-    public getColorKey(databaseName: string, tableName: string, fieldName: string): string {
-        return this._service.getColorKey(databaseName, tableName, fieldName);
-    }
-
-    public getColorSet(colorKey: string): ColorSet {
-        return this._service.getColorSet(colorKey);
-    }
-
-    public getTheme(): string {
-        return this._service.getTheme();
-    }
-
-    public getThemes(): Theme[] {
-        return this._service.getThemes();
-    }
-
-    public getThemeAccentColorHex(): string {
-        return this._service.getThemeAccentColorHex();
-    }
-
-    public getThemeMainColorHex(): string {
-        return this._service.getThemeMainColorHex();
-    }
-
-    public initializeColors(colors: Record<string, Record<string, Record<string, Record<string, string>>>>): void {
-        this._service.initializeColors(colors);
-    }
-
-    public setTheme(id: string): void {
-        this._service.setTheme(id);
-    }
-}
