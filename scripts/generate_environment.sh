1b50d8a04949b51099e23e07c5bf82c22d7b13c6
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-07-10 20:45:07 -0400

    THOR-1011 Always recreate environment files.  Changed date string format and fixed space issue.

--- scripts/generate_environment.sh
+++ scripts/generate_environment.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Creation of environment files
-if [[ ! -e src/environments/environment.ts || ! -e src/environments/environment.prod.ts ]] ; then
-  cp src/environments/environment_config.ts src/environments/environment.ts
-  cp src/environments/environment_config.ts src/environments/environment.prod.ts
-fi
-DATE=$(date +"%b-%d,%Y--%H:%M%P")
-BUILD_DATE="buildDate:'${DATE}',"
+cp src/environments/environment_config.ts src/environments/environment.ts
+cp src/environments/environment_config.ts src/environments/environment.prod.ts
+
+DATE=$(date +"%B %d, %Y at %H:%M")
+BUILD_DATE="buildDate: '${DATE}',"
 RECENT_GIT=$(git log --format="%H" -n 1)
-RECENT_GIT_STRING="recentGit:'${RECENT_GIT}'"
+RECENT_GIT_STRING="recentGit: '${RECENT_GIT}'"
 
 # Replacing stubs of Environment variables
 # Development Environment:
-npx replace-in-file /buildDate.*/ ${BUILD_DATE} src/environments/environment.ts --isRegex
-npx replace-in-file /recentGit.*/ ${RECENT_GIT_STRING} src/environments/environment.ts --isRegex
+npx replace-in-file /buildDate.*/ "${BUILD_DATE}" src/environments/environment.ts --isRegex
+npx replace-in-file /recentGit.*/ "${RECENT_GIT_STRING}" src/environments/environment.ts --isRegex
 # Production Environemnt:
-npx replace-in-file /production.*/ production:true, src/environments/environment.prod.ts --isRegex
-npx replace-in-file /buildDate.*/ ${BUILD_DATE} src/environments/environment.prod.ts --isRegex
-npx replace-in-file /recentGit.*/ ${RECENT_GIT_STRING} src/environments/environment.prod.ts --isRegex
+npx replace-in-file /production.*/ "production: true", src/environments/environment.prod.ts --isRegex
+npx replace-in-file /buildDate.*/ "${BUILD_DATE}" src/environments/environment.prod.ts --isRegex
+npx replace-in-file /recentGit.*/ "${RECENT_GIT_STRING}" src/environments/environment.prod.ts --isRegex
