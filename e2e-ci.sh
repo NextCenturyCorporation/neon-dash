1850ccdfde94c0fd2ad79f9f64c4d05942f5a3d8
Timothy Soehnlin <tim.soehnlin@nextcentury.com>
2019-07-24 17:13:04 -0400

    Fixing issue with hanging

--- e2e-ci.sh
+++ e2e-ci.sh
@@ -2,8 +2,6 @@
 
 set +e
 
-trap 'kill $(jobs -p) 0 || true' EXIT TERM
-
 ls node_modules/protractor/node_modules/webdriver-manager/selenium || npx webdriver-manager update
 
 cp src/app/config/cicd/lorelei.config.yaml src/app/config/config.yaml
@@ -14,6 +12,4 @@ node e2e/ci.server.js &
 
 E2E_JUNIT=1 npx protractor e2e/protractor.conf.js
 
-kill $(jobs -p) 0 || true
-
-exit 0
+kill $(jobs -p) 0 2> /dev/null
