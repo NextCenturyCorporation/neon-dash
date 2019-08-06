723f1febafc4f4e9efea7627a199eb05b70dd9a1
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-08-05 21:49:09 -0400

    Re-added WAR build script.

---
+++ scripts/build_dashboard.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A script to run build commands on custom arguements and update build metadata
+
+# Done for all builds
+BUILD=${1:-dash}
+rm -f target/*.war && mkdir -p target
+ng build --deployUrl=/${BUILD}/ --base-href /${BUILD}/ --aot --prod
+cd dist && zip -r ${BUILD}.war * && cd .. && mv dist/${BUILD}.war target/${BUILD}.war
