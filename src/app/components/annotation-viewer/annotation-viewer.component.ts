942e923f124ecf134f8ecca142d4c16d8dd235b1
Nic Brockman <nicolas.brockman@nextcentury.com>
2019-08-09 15:35:40 -0400

    URLs work in the annotation viewer

--- src/app/components/annotation-viewer/annotation-viewer.component.ts
+++ src/app/components/annotation-viewer/annotation-viewer.component.ts
@@ -107,6 +107,7 @@ export class AnnotationViewerComponent extends BaseNeonComponent implements OnIn
     public colorKeys: string[] = [];
     public indexInclusive: boolean;
     public offset = 0;
+    public url: string = '';
 
     constructor(
         protected colorThemeService: InjectableColorThemeService,
@@ -714,5 +715,22 @@ export class AnnotationViewerComponent extends BaseNeonComponent implements OnIn
 
     showLegendContainer(): boolean {
         return (!this.options.singleColor && this.colorKeys.length > 0);
+    }
+
+    removeUrl(text: string) {
+        return neonUtilities.removeUrl(text);
+    }
+
+    hasUrl(text: string) {
+        let urlPattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
+        let prefixPattern = new RegExp('^(http|https)://');
+        let test = urlPattern.test(text);
+        if (test) {
+            this.url = neonUtilities.checkStringForUrl(text);
+            if (!prefixPattern.test(this.url)) {
+                this.url = 'http://' + this.url;
+            }
+        }
+        return test;
     }
 }
