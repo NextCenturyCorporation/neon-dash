ccd3a5bffdef20e60a6b6e14e67104ccb95e423b
Nic Brockman <nicolas.brockman@nextcentury.com>
2019-08-09 14:00:36 -0400

    Links converted to anchors in newsfeed

--- src/app/components/news-feed/news-feed.component.ts
+++ src/app/components/news-feed/news-feed.component.ts
@@ -62,6 +62,8 @@ export class NewsFeedComponent extends BaseNeonComponent implements OnInit, OnDe
     @ViewChild(MatAccordion) accordion: MatAccordion;
 
     public newsFeedData: any[] = null;
+    public selectedItem: object = undefined;
+    public url: string = '';
 
     constructor(
         dashboardService: DashboardService,
@@ -324,6 +326,7 @@ export class NewsFeedComponent extends BaseNeonComponent implements OnInit, OnDe
      * @private
      */
     selectItem(item) {
+        this.selectedItem = item;
         if (this.options.idField.columnName) {
             this.publishSelectId(item[this.options.idField.columnName]);
         }
@@ -356,5 +359,23 @@ export class NewsFeedComponent extends BaseNeonComponent implements OnInit, OnDe
      */
     isSelected(item) {
         return item._filtered;
+    }
+
+    isExpanded(item) {
+        return this.selectedItem === item;
+    }
+
+    checkStringForUrl(text: string) {
+        return neonUtilities.checkStringForUrl(text);
+    }
+
+    removeUrl(text: string) {
+        return neonUtilities.removeUrl(text);
+    }
+
+    hasUrl(text: string) {
+        let urlPattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
+        this.url = neonUtilities.checkStringForUrl(text);
+        return urlPattern.test(text);
     }
 }
