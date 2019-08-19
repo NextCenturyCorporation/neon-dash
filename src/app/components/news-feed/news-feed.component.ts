01e56bbbff948754a793e31b7914070a44630a35
Nic Brockman <nicolas.brockman@nextcentury.com>
2019-08-19 16:11:39 -0400

    Fixed Text Url bug and consolidated function into neonUtilities for none duplicated tests in future

--- src/app/components/news-feed/news-feed.component.ts
+++ src/app/components/news-feed/news-feed.component.ts
@@ -367,18 +367,10 @@ export class NewsFeedComponent extends BaseNeonComponent implements OnInit, OnDe
         return this.selectedItem === item;
     }
 
-    removeUrl(text: string) {
-        return neonUtilities.removeUrl(text);
-    }
-
     hasUrl(text: string) {
-        // eslint-disable-next-line max-len
-        let urlPattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
-        let test = urlPattern.test(text);
-        if (test) {
-            this.url = neonUtilities.checkStringForUrl(text);
-            this.text = neonUtilities.splitStringByUrl(text);
-        }
-        return test;
+        let textObject = neonUtilities.hasUrl(text);
+        this.url = textObject.url;
+        this.text = textObject.splitText;
+        return textObject.test;
     }
 }
