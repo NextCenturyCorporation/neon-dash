376c86364e5964280b35a0019fffe1e41d3cfadc
KBai <kevin.baik@nextcentury.com>
2019-09-11 14:10:29 -0400

    Added non-https case

--- src/app/components/wiki-viewer/wiki-viewer.component.ts
+++ src/app/components/wiki-viewer/wiki-viewer.component.ts
@@ -61,7 +61,8 @@ export class WikiData {
 export class WikiViewerComponent extends BaseNeonComponent implements OnInit, OnDestroy {
     static WIKI_LINK_PREFIX_TITLE: string = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&prop=text&page=';
     static WIKI_LINK_PREFIX_ID: string = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&prop=text&pageid=';
-    static WIKI_LINK_PREFIX_URL: string = 'https://en.wikipedia.org/wiki/';
+    static WIKI_LINK_PREFIX_URL_HTTPS: string = 'https://en.wikipedia.org/wiki/';
+    static WIKI_LINK_PREFIX_URL: string = 'en.wikipedia.org/wiki/';
     static WIKI_LINK_PREFIX_WIKI: string = '/wiki/';
     static WIKI_LINK_PREFIX_WIKI_FULL: string = 'https://en.wikipedia.org/wiki/';
     @ViewChild('headerText') headerText: ElementRef;
@@ -241,7 +242,9 @@ export class WikiViewerComponent extends BaseNeonComponent implements OnInit, On
             try {
                 let links = neonUtilities.deepFind(results[0], options.linkField.columnName) || [];
                 links = (Array.isArray(links) ? links : [links]).map((link) => {
-                    if (!this.options.useWikipediaPageID && link.includes(WikiViewerComponent.WIKI_LINK_PREFIX_URL)) {
+                    if (!this.options.useWikipediaPageID && link.includes(WikiViewerComponent.WIKI_LINK_PREFIX_URL_HTTPS)) {
+                        return link.substring(WikiViewerComponent.WIKI_LINK_PREFIX_URL_HTTPS.length);
+                    } else if (!this.options.useWikipediaPageID && link.includes(WikiViewerComponent.WIKI_LINK_PREFIX_URL)) {
                         return link.substring(WikiViewerComponent.WIKI_LINK_PREFIX_URL.length);
                     }
                     return link;
