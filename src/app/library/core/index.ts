a07e17448deefaded6b92e72dfb6496064c430f5
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-09-17 09:35:11 -0400

    THOR-1424 Added README, LICENSE, index, package, and tsconfig files to core component library so it can be published to NPM in the future.

---
+++ src/app/library/core/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2019 Next Century Corporation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import './core.util';
+import './date.util';
+import './components/aggregation.webcomponent';
+import './components/element.webcomponent';
+import './components/filter.webcomponent';
+import './components/group.webcomponent';
+import './components/search.webcomponent';
+import './services/abstract.search.service.webcomponent';
+import './services/abstract.color-theme.service.webcomponent';
+import './services/connection.service.webcomponent';
+import './services/filter.service.webcomponent';
+import './services/search.service.webcomponent';
+import './models/dataset';
+import './models/filters';
+import './models/widget-option';
+
