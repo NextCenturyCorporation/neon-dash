181aed8d4640a8b1d5cf6f336701ab99ca930d7f
Akmal Amduka <akmal.amduka@nextcentury.com>
2019-09-30 21:10:39 -0400

    export to csv feature UI update

--- src/app/components/export-control/export-control.component.ts
+++ src/app/components/export-control/export-control.component.ts
@@ -33,9 +33,6 @@ export class ExportControlComponent {
     public exportFormatList: any[] = [{
         name: 'csv',
         value: 0
-    }, {
-        name: 'xlsx',
-        value: 1
     }];
 
     public exportFormat: number;
@@ -67,10 +64,12 @@ export class ExportControlComponent {
     }
 
     exportSuccess(queryResults) {
-        this.messenger.publish(neonEvents.DASHBOARD_MESSAGE, {
-            message: 'Exporting...'
-        });
-        window.location.assign('/neon/services/exportservice/generateZip/' + queryResults.data);
+        let link = document.createElement('a');
+        link.href = 'data:text/csv;charset=utf-8,' + encodeURI(queryResults.data);
+        link.target = '_blank';
+        link.download = queryResults.fileName;
+        link.click();
+        link.remove();
     }
 
     exportFail(response) {
@@ -114,9 +113,14 @@ export class ExportControlComponent {
             });
             return;
         }
-        if (data && data.data && data.data.length === 1) {
+
+        /*
+        If (data && data.data && data.data.length === 1) {
             data.name = data.data[0].name;
-        }
-        connection.runExportQuery(data, this.exportFormat, this.exportSuccess.bind(this), this.exportFail.bind(this));
+        }*/
+
+        let exportData = data.data[0];
+        let exportFormatName = this.exportFormatList[this.exportFormat].name;
+        connection.runExportQuery(exportData, exportFormatName, this.exportSuccess.bind(this), this.exportFail.bind(this));
     }
 }
