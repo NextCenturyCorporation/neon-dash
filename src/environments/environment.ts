3cc7310207e5a270c3823358cafc13ebc342e95e
Darian <darian.garcia@nextcentury.com>
2019-10-10 10:54:03 -0400

    fixing timing formats

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,9 +12,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+function buildDate(){
+    let bDate: string = new Date().toUTCString();
+    var bDate_split = bDate.split(" ");
+    var emptyDate = '';
+    var retDate = emptyDate.concat(bDate_split[2], ' ', bDate_split[1], ', ', bDate_split[3], ', ', bDate_split[4], ' ', bDate_split[5])
+    return retDate;
+}
+
 export const environment = {
     config: ['./app/config/config.yaml', './app/config/config.json'],
     production: false,
-    buildDate: new Date().toUTCString(),
+    buildDate: buildDate(),
     recentGit: 'HEAD'
 };
