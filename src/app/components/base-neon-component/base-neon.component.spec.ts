e473b3837bf33b7199e5d001a7b46c598b37ebb1
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-11-08 14:18:34 -0500

    THOR-1181 Moved SingleVisualizationWidgetComponent files and related changes from branch THOR-1180 for a CR.

--- src/app/components/base-neon-component/base-neon.component.spec.ts
+++ src/app/components/base-neon-component/base-neon.component.spec.ts
@@ -25,14 +25,14 @@ import {
 
 import { BaseNeonComponent } from '../base-neon-component/base-neon.component';
 
-import { AbstractSearchService } from '../../library/core/services/abstract.search.service';
+import { AbstractSearchService } from 'component-library/dist/core/services/abstract.search.service';
 import { DashboardService } from '../../services/dashboard.service';
-import { AbstractFilterDesign, FilterCollection, ListFilter } from '../../library/core/models/filters';
+import { AbstractFilterDesign, FilterCollection, ListFilter } from 'component-library/dist/core/models/filters';
 import { InjectableFilterService } from '../../services/injectable.filter.service';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NeonConfig } from '../../models/types';
-import { FieldConfig } from '../../library/core/models/dataset';
+import { FieldConfig } from 'component-library/dist/core/models/dataset';
 import {
     AggregationType,
     CompoundFilterType,
@@ -44,11 +44,11 @@ import {
     ConfigOptionNonPrimitive,
     ConfigOptionSelect,
     OptionChoices
-} from '../../library/core/models/config-option';
+} from 'component-library/dist/core/models/config-option';
 import { WidgetOptionCollection } from '../../models/widget-option-collection';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { DashboardServiceMock } from '../../services/mock.dashboard-service';
-import { SearchServiceMock } from '../../library/core/services/mock.search.service';
+import { SearchServiceMock } from 'component-library/dist/core/services/mock.search.service';
 import { initializeTestBed, getConfigService } from '../../../testUtils/initializeTestBed';
 import { neonEvents } from '../../models/neon-namespaces';
 import { MatDialog, MatDialogModule } from '@angular/material';
@@ -1313,22 +1313,6 @@ describe('BaseNeonComponent', () => {
         component['handleTransformVisualizationQueryResults'](expectedOptions, expectedResults, successCallback, failureCallback);
     });
 
-    it('isNumber does return expected boolean', () => {
-        expect(component.isNumber(true)).toEqual(false);
-        expect(component.isNumber('a')).toEqual(false);
-        expect(component.isNumber([1, 2])).toEqual(false);
-        expect(component.isNumber({
-            value: 1
-        })).toEqual(false);
-
-        expect(component.isNumber(1)).toEqual(true);
-        expect(component.isNumber(12.34)).toEqual(true);
-        expect(component.isNumber(-12.34)).toEqual(true);
-        expect(component.isNumber('1')).toEqual(true);
-        expect(component.isNumber('12.34')).toEqual(true);
-        expect(component.isNumber('-12.34')).toEqual(true);
-    });
-
     it('onResizeStop works as expected', () => {
         let spyUpdateHeader = spyOn(component, 'updateHeaderTextStyles');
         let spyUpdateSubclass = spyOn(component, 'updateOnResize');
@@ -1351,15 +1335,6 @@ describe('BaseNeonComponent', () => {
             expect(spyRefreshVisualization.calls.count()).toEqual(1);
             done();
         }, 500);
-    });
-
-    it('prettifyInteger does return expected string', () => {
-        expect(component.prettifyInteger(0)).toEqual('0');
-        expect(component.prettifyInteger(1)).toEqual('1');
-        expect(component.prettifyInteger(12)).toEqual('12');
-        expect(component.prettifyInteger(123)).toEqual('123');
-        expect(component.prettifyInteger(1234)).toEqual('1,234');
-        expect(component.prettifyInteger(1234567890)).toEqual('1,234,567,890');
     });
 
     it('shouldFilterSelf does return expected boolean', () => {
