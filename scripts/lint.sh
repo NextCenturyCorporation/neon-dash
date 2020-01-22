2f7e08a73e6472934dd5988c7b2f8399676fcc96
Scott Warner <scott.warner@caci.com>
2020-01-22 15:49:50 -0500

    Use an error exit code if a lint script fails

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-$BASEDIR/lint_ts.sh
-$BASEDIR/lint_sass.sh
-$BASEDIR/lint_html.sh
+EXIT_STATUS=0
+$BASEDIR/lint_ts.sh || EXIT_STATUS=$?
+$BASEDIR/lint_sass.sh || EXIT_STATUS=$?
+$BASEDIR/lint_html.sh || EXIT_STATUS=$?
+
+exit $EXIT_STATUS
