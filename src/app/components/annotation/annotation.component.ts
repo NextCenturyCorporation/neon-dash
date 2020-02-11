847d95469c228e801764e861c758cd11710f2472
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2020-02-11 11:41:36 -0500

    Updated annotation component functionality and styling.

--- src/app/components/annotation/annotation.component.ts
+++ src/app/components/annotation/annotation.component.ts
@@ -12,12 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, Inject } from '@angular/core';
+import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
 import { ConfigService } from '../../services/config.service';
 import { InjectableConnectionService } from '../../services/injectable.connection.service';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatSelect, MAT_DIALOG_DATA } from '@angular/material';
 import { NeonConfig } from '../../models/types';
-import { FormGroup, FormBuilder } from '@angular/forms';
 import {
     BoundsFilterDesign,
     DatabaseConfig,
@@ -34,107 +33,93 @@ import { neonEvents } from '../../models/neon-namespaces';
     templateUrl: 'annotation.component.html',
     styleUrls: ['annotation.component.scss']
 })
-export class AnnotationComponent {
-    public confirmDialogRef: any;
-    public confirmMessage: string = '';
-    public target: string;
-    public title: string = 'Update';
-    public confirmText: string = 'Update Label';
-    public cancelText: string = 'Cancel';
+export class AnnotationComponent implements AfterViewInit {
+    @ViewChild('annotationDropdown', { static: true }) annotationDropdown: MatSelect;
+
     public labels: any[];
+    public datastore: DatastoreConfig;
+    public database: DatabaseConfig;
+    public table: TableConfig;
+    public labelField: FieldConfig;
+    public idField: FieldConfig;
+    public dataId: string|number;
+    public defaultLabel: string;
+    public selectedLabel: string;
 
-    private _datastore: DatastoreConfig;
-    private _database: DatabaseConfig;
-    private _table: TableConfig;
-    private _labelField: FieldConfig;
-    private _idField: FieldConfig;
-    private _dataId: string|number;
-    private _defaultLabel: string;
-    private selectedLabel: string;
     private _messenger: eventing.Messenger;
-
-    public onChangeName() {
-        let fieldsWithValues = {};
-        fieldsWithValues[this._labelField.columnName] = this.selectedLabel;
-
-        let connection = this.connectionService.connect(this._datastore.type, this._datastore.host);
-
-        connection.runMutate({
-            datastoreHost: this._datastore.host,
-            datastoreType: this._datastore.type,
-            databaseName: this._database.name,
-            tableName: this._table.name,
-            idFieldName: this._idField.columnName,
-            dataId: this._dataId,
-            fieldsWithValues
-        }, ((response: any) => {
-            if (response.error) {
-                this._onError(response.error);
-            } else {
-                this._onSuccess(response.success);
-            }
-        }), this._onError.bind(this));
-    }
-
-    private _closeDialog() {
-        this.dialogRef.close({data:this.selectedLabel});
-    }
-
-    private _onError(error: any) {
-        this._messenger.publish(neonEvents.DASHBOARD_MESSAGE, {
-            error,
-            message: 'Annotation operation failed on ' + this._datastore.name + '.' + this._database.name + '.' + this._table.name +
-                '.' + this._labelField.columnName + ' = ' + this.selectedLabel
-        });
-        this._closeDialog();
-    }
-
-    private _onSuccess(message: string) {
-        this._messenger.publish(neonEvents.DASHBOARD_MESSAGE, {
-            message
-        });
-        this._closeDialog();
-    }
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: any,
         public dialogRef: MatDialogRef<AnnotationComponent>,
         private configService: ConfigService,
-        private connectionService: InjectableConnectionService,
-        private fb: FormBuilder
+        private connectionService: InjectableConnectionService
     ) {
         this._messenger = new eventing.Messenger();
-        this.target = data.target;
         this.title = data.title || this.title;
         this.confirmText = data.confirmText || this.confirmText;
         this.cancelText = data.cancelText || this.cancelText;
-        this._datastore = data.datastore;
-        this._database = data.database;
-        this._table = data.table;
-        this._labelField = data.labelField;
-        this._idField = data.idField;
-        this._dataId = data.dataId;
-        this._defaultLabel = data.defaultLabel;
-        //console.log(data.defaultLabel);
-        this.selectedLabel = this._defaultLabel;
-        // this.configService.getActive().subscribe((neonConfig: NeonConfig) => {
-        //     this.labels = neonConfig.dataLabels.sort(function (a, b) {
-        //         if (a > b) {
-        //             return 1;
-        //         }
-        //         if (b > a) {
-        //             return -1;
-        //         }
-        //         return 0;
-        //     });
-        // });
+        this.datastore = data.datastore;
+        this.database = data.database;
+        this.table = data.table;
+        this.labelField = data.labelField;
+        this.idField = data.idField;
+        this.dataId = data.dataId;
+        this.defaultLabel = data.defaultLabel;
+        this.selectedLabel = this.defaultLabel;
+
         this.configService.getActive().subscribe((neonConfig: NeonConfig) => {
             this.labels = neonConfig.dataLabels;
         });
-
-        // this.labels = this.fb.group({
-        //     defaultLabel: this._defaultLabel
-        // });
     }
 
+    public ngAfterViewInit(): void {
+        // This is a hack so the dropdown will auto-scroll to the selected item because we've reduced the option height in our custom CSS.
+        // https://github.com/angular/components/issues/3419#issuecomment-391007003
+        this.annotationDropdown['_getItemHeight'] = () => 30;
+    }
+
+    public saveAnnotation(): void {
+        let fieldsWithValues = {};
+        fieldsWithValues[this.labelField.columnName] = this.selectedLabel;
+
+        let connection = this.connectionService.connect(this.datastore.type, this.datastore.host);
+
+        connection.runMutate({
+            datastoreHost: this.datastore.host,
+            datastoreType: this.datastore.type,
+            databaseName: this.database.name,
+            tableName: this.table.name,
+            idFieldName: this.idField.columnName,
+            dataId: this.dataId,
+            fieldsWithValues
+        }, ((response: any) => {
+            if (response.error) {
+                this._onError(response.error);
+            } else {
+                this._onSuccess();
+            }
+        }), this._onError.bind(this));
+    }
+
+    private _closeDialog(): void {
+        this.dialogRef.close({
+            data:this.selectedLabel
+        });
+    }
+
+    private _onError(error: any): void {
+        this._messenger.publish(neonEvents.DASHBOARD_MESSAGE, {
+            error,
+            message: 'Annotation operation failed on ' + this.datastore.name + '.' + this.database.name + '.' + this.table.name +
+                '.' + this.labelField.columnName + ' = ' + this.selectedLabel
+        });
+        this._closeDialog();
+    }
+
+    private _onSuccess(): void {
+        this._messenger.publish(neonEvents.DASHBOARD_MESSAGE, {
+            message: 'Annotation successful!'
+        });
+        this._closeDialog();
+    }
 }
