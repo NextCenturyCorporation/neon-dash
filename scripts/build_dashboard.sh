28c32f506c316edb2fca407e4fbeafaaba3efa5c
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2020-06-28 14:22:25 -0400

    Added log message to build script.

--- scripts/build_dashboard.sh
+++ scripts/build_dashboard.sh
@@ -3,6 +3,7 @@
 
 # Done for all builds
 BUILD=${1:-dash}
+echo "Building dashboard with base-href and deployUrl of ${BUILD}"
 rm -f target/*.war && mkdir -p target
 ng build --aot --deployUrl=/${BUILD}/ --base-href /${BUILD}/ -c custom
 cd dist && zip -r ${BUILD}.war * && cd .. && mv dist/${BUILD}.war target/${BUILD}.war
